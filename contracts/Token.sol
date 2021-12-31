faedae1205f66d86d9aca7289388370a85664610
Garima Aggarwal <garimaaggarwal@Garimas-MacBook-Pro.local>
2021-12-30 22:32:54 -0500

    adding code to github

---
+++ contracts/Token.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token{
+    string public name = 'Test Token';
+    string public symbol = 'TT';
+    uint public totalSupply = 100000;
+    mapping(address => uint) balances;
+    constructor(){
+        balances[msg.sender] = totalSupply;
+    }
+    function transfer(address to, uint amount) external{
+        require(balances[msg.sender]>= amount,'Not enough tokens');
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+    function balanceOf(address account) external view returns (uint){
+
+        return balances[account];
+    }
+
+
+}
